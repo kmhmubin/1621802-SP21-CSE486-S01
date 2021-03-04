41752733e6165e08a1910231a42613d311a673d3
K M H Mubin <kmh.mubin@northsouth.edu>
2021-03-04 16:50:53 +0600

    lab-02 #26: show the message in the second activity screen
    add an text field to write message
    show message in the second activity

--- Lab/Lab02/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ Lab/Lab02/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -6,20 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // get the reference of the message
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button Clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
+        // get the string value from the edit text field
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
