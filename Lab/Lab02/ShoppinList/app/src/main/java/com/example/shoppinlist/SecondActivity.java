4d0eecba0db8a603a8ebd7281ff1ae9329ae4150
K M H Mubin <kmh.mubin@northsouth.edu>
2021-03-07 00:01:55 +0600

    feature #26: add reference value for every buttons

--- Lab/Lab02/ShoppinList/app/src/main/java/com/example/shoppinlist/SecondActivity.java
+++ Lab/Lab02/ShoppinList/app/src/main/java/com/example/shoppinlist/SecondActivity.java
@@ -2,10 +2,15 @@ package com.example.shoppinlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_ITEMS = "com.example.shoppinlist.extra.ITEMS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,59 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
     }
 
+    @SuppressLint("NonConstantResourceId")
     public void selectItem(View view) {
+        Intent return_items_intent = new Intent();
+        switch (view.getId()) {
+            case R.id.button_apple:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Apple");
+                setResult(RESULT_OK, return_items_intent);
+                finish();
+                break;
+            case R.id.button_bread:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Bread");
+                setResult(RESULT_OK, return_items_intent);
+                finish();
+                break;
+            case R.id.button_cheese:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Cheese");
+                setResult(RESULT_OK, return_items_intent);
+                finish();
+                break;
+            case R.id.button_eggs:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Eggs");
+                setResult(RESULT_OK, return_items_intent);
+                break;
+            case R.id.button_milk:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Milk");
+                setResult(RESULT_OK, return_items_intent);
+                break;
+            case R.id.button_oil:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Oil");
+                setResult(RESULT_OK, return_items_intent);
+                break;
+            case R.id.button_rice:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Rice");
+                setResult(RESULT_OK, return_items_intent);
+                break;
+            case R.id.button_salt:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Salt");
+                setResult(RESULT_OK, return_items_intent);
+                finish();
+                break;
+            case R.id.button_sugar:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Suger");
+                setResult(RESULT_OK, return_items_intent);
+                finish();
+                break;
+            case R.id.button_wheat:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Wheat");
+                setResult(RESULT_OK, return_items_intent);
+                finish();
+                break;
+            default:
+                Toast.makeText(SecondActivity.this, "Error!!", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
