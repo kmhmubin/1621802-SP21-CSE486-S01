f1199387f0fa6ca47b615c4ba566aa92228e6833
K M H Mubin <kmh.mubin@northsouth.edu>
2021-03-17 16:40:40 +0600

    Lab #32: Added functionality for Add Items button and show text view

--- Lab/Lab03/ShoppingListii/app/src/main/java/com/example/shoppinglistii/MainActivity.java
+++ Lab/Lab03/ShoppingListii/app/src/main/java/com/example/shoppinglistii/MainActivity.java
@@ -1,19 +1,91 @@
 package com.example.shoppinglistii;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // default the item 1
+    public static final int ITEM_REQUEST = 1;
+    // assign 10 empty text view in a array
+    private final TextView[] item = new TextView[10];
+    // creating array list of items
+    private ArrayList<String> itemList = new ArrayList<>(10);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // assigning the empty text view items with array item list by id
+        item[0] = findViewById(R.id.item1);
+        item[1] = findViewById(R.id.item2);
+        item[2] = findViewById(R.id.item3);
+        item[3] = findViewById(R.id.item4);
+        item[4] = findViewById(R.id.item5);
+        item[5] = findViewById(R.id.item6);
+        item[6] = findViewById(R.id.item7);
+        item[7] = findViewById(R.id.item8);
+        item[8] = findViewById(R.id.item9);
+        item[9] = findViewById(R.id.item10);
+
+        if (savedInstanceState != null) {
+            itemList = savedInstanceState.getStringArrayList("ItemListStringArray");
+            if (itemList != null && itemList.size() > 0) {
+                for (int i = 0; i < itemList.size(); i++) {
+                    if (itemList.size() > 10) {
+                        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    item[i].setVisibility(View.VISIBLE);
+                    item[i].setText(itemList.get(i));
+                }
+            }
+        }
+
+    }
+
+    // Save the Activity instance state with onSaveInstanceState
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (itemList.size() != 0) {
+            outState.putStringArrayList("ItemListStringArray", itemList);
+        }
     }
 
     public void addItem(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, ITEM_REQUEST);
+    }
+
+    // from second activity to main activity
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ITEM_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String itemString = data.getStringExtra(SecondActivity.EXTRA_ITEMS);
+                itemList.add(itemString);
+                for (int i = 0; i < itemList.size(); i++) {
+                    if (itemList.size() > 10) {
+                        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    item[i].setVisibility(View.VISIBLE);
+                    item[i].setText(itemList.get(i));
+                }
+            }
+        }
     }
 
     public void searchStore(View view) {
