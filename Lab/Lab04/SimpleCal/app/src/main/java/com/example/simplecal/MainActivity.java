d4cb969995b83a5896fc20f05a2d1dabf414a841
K M H Mubin <kmh.mubin@northsouth.edu>
2021-03-25 21:01:45 +0600

    style #33: Design the UI in Linear Layout

--- Lab/Lab04/SimpleCal/app/src/main/java/com/example/simplecal/MainActivity.java
+++ Lab/Lab04/SimpleCal/app/src/main/java/com/example/simplecal/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.simplecal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAdd(View view) {
+    }
+
+    public void onSub(View view) {
+    }
+
+    public void onDiv(View view) {
+    }
+
+    public void onMul(View view) {
     }
 }
