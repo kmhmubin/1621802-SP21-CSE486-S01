767c906d84428ad125fa00bedc38c44094a716c1
K M H Mubin <kmh.mubin@northsouth.edu>
2021-03-25 22:11:46 +0600

    test #33: add two number test pass

--- Lab/Lab04/SimpleCal/app/src/test/java/com/example/simplecal/ExampleUnitTest.java
+++ Lab/Lab04/SimpleCal/app/src/test/java/com/example/simplecal/ExampleUnitTest.java
@@ -1,7 +1,12 @@
 package com.example.simplecal;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 /**
@@ -9,9 +14,21 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(JUnit4.class)
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+   private Calculator mCalculator;
+
+   // setting up the environment
+    @Before
+    public void setUp(){
+        mCalculator = new Calculator();
     }
+
+    // testing the addition
+    @Test
+    public void addTwoNumbers(){
+        double resultAdd = mCalculator.addition(1d,1d);
+        assertThat(resultAdd, is(equalTo(2d)));
+    }
+
 }
