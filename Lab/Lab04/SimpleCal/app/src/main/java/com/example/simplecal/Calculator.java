171e9b937e52148eeaa2c097924b79904d04b40a
K M H Mubin <kmh.mubin@northsouth.edu>
2021-03-25 21:14:23 +0600

    #33: add calculator class for calculation operation
    Operations are:
    - addition
    - subtraction
    - multiplication
    - division

--- Lab/Lab04/SimpleCal/app/src/main/java/com/example/simplecal/Calculator.java
+++ Lab/Lab04/SimpleCal/app/src/main/java/com/example/simplecal/Calculator.java
@@ -1,4 +1,26 @@
 package com.example.simplecal;
 
 public class Calculator {
+    // calculation operation
+    public enum Operator {ADD, SUB, DIV, MUL}
+
+    // Addition
+    public double addition(double A, double B) {
+        return A + B;
+    }
+
+    // Subtract
+    public double subtraction(double A, double B) {
+        return A - B;
+    }
+
+    // Divide
+    public double division(double A, double B) {
+        return A / B;
+    }
+
+    // Multiply
+    public double multiply(double A, double B) {
+        return A * B;
+    }
 }
