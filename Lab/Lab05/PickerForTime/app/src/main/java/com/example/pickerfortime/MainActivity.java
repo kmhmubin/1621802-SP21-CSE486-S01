0cd97dc775195ead3e770b93f642bd6169d44079
K M H Mubin <kmh.mubin@northsouth.edu>
2021-03-31 23:46:08 +0600

    feat #42: Show Time Picker Dialog and replace text with time

--- Lab/Lab05/PickerForTime/app/src/main/java/com/example/pickerfortime/MainActivity.java
+++ Lab/Lab05/PickerForTime/app/src/main/java/com/example/pickerfortime/MainActivity.java
@@ -1,21 +1,46 @@
 package com.example.pickerfortime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.text.BreakIterator;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int hour, minute;
+    private Button timeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timeButton = findViewById(R.id.time_button);
     }
 
     public void showTimePicker(View view) {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int selectMinute) {
+                hour = hourOfDay;
+                minute = selectMinute;
+                timeButton.setText(String.format(Locale.getDefault(), "%02d:%2d", hour, minute));
+            }
+        };
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
     }
 
     public void showDatePicker(View view) {
     }
+
+
 }
