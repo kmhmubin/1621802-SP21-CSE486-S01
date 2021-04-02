a9a8a88cecebbf2ab7754ae6519a219243b5dbf9
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-02 16:52:10 +0600

    feat #42: add click listener for login
    Successful login:
    - show alert dialog with welcome message
    
    Error:
    - show a toast message

--- Lab/Lab05/LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
+++ Lab/Lab05/LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.loginapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // variables for the input fields and button
@@ -22,6 +26,30 @@ public class MainActivity extends AppCompatActivity {
         btLogin = findViewById(R.id.login_button);
 
         // adding a click listener for button
-        
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // button clicked or not
+                if (etUsername.getText().toString().equals("admin") && etPassword.getText().toString().equals("password")) {
+                    // adding alert dialog if successful login
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    // adding icon on alert
+                    builder.setIcon(R.drawable.ic_outline_check_circle_outline_24);
+                    builder.setTitle("Login Successful");
+                    builder.setMessage("Welcome Admin");
+                    builder.setNegativeButton("YES", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                } else {
+                    // show toast message if failed
+                    Toast.makeText(getApplicationContext(), "Invalid username and password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
