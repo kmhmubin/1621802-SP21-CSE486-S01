19ed82ee2b2ec76512e7824c2d8aa3454a232fb6
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-02 16:38:41 +0600

    feat #42: add the reference for the style

--- Lab/Lab05/LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
+++ Lab/Lab05/LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    // variables for the input fields and button
+    EditText etUsername, etPassword;
+    Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // reference the input fields and button with xml
+        etUsername = findViewById(R.id.username);
+        etPassword = findViewById(R.id.password);
+        btLogin = findViewById(R.id.login_button);
+
+        // adding a click listener for button
+        
     }
 }
