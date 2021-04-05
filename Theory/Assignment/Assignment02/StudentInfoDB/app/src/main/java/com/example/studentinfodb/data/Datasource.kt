d3280e79a3f5f68b720dea513e42d860729cdb1d
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-05 13:13:11 +0600

    format #35: data source class code re-format

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/data/Datasource.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/data/Datasource.kt
@@ -4,8 +4,8 @@ import com.example.studentinfodb.R
 import com.example.studentinfodb.model.Student
 
 class Datasource {
-//  return a list of students
-    fun loadStudents():List<Student>{
+    //  return a list of students
+    fun loadStudents(): List<Student> {
         return listOf<Student>(
             Student(R.string.student1),
             Student(R.string.student2),
@@ -16,7 +16,7 @@ class Datasource {
             Student(R.string.student7),
             Student(R.string.student8),
             Student(R.string.student9),
-            Student(R.string.student10),
+            Student(R.string.student10)
         )
     }
 }
