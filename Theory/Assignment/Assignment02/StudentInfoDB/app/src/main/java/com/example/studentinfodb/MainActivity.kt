81a022350ae2c45bb5c3688810ea9c92cb081fe3
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-05 13:19:31 +0600

    style #35: add Recycle view to the layout

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.studentinfodb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.studentinfodb.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
