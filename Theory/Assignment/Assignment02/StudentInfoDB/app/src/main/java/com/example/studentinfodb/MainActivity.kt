c89b38aed17f12df0366ec1d3088e72e9c6c216c
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-05 14:46:55 +0600

    style #35: add text view

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.studentinfodb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studentinfodb.adapter.ItemAdapter
 import com.example.studentinfodb.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // initialize the data source
+        val myDataSource = Datasource().loadStudents()
+
+        // add reference for the recycle view
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataSource)
+        // improve performance for fixed dataset
+        recyclerView.setHasFixedSize(true)
 
     }
 }
