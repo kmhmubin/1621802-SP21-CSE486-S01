5256febbdc26c73a4b61393898ed32bfae486838
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-07 22:33:17 +0600

    feat #35: add a floating button for add new student info

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
@@ -1,25 +1,37 @@
 package com.example.studentinfodb
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.widget.TextView
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.studentinfodb.adapter.ItemAdapter
 import com.example.studentinfodb.data.Datasource
+import com.example.studentinfodb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // change into view binding
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // initialize the data source
         val myDataSource = Datasource().loadStudents()
 
         // add reference for the recycle view
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView = binding.recyclerView
         recyclerView.adapter = ItemAdapter(this, myDataSource)
         // improve performance for fixed dataset
         recyclerView.setHasFixedSize(true)
 
+
+    }
+
+    fun fabButton(view: View) {
+        val intent = Intent(applicationContext, InsertStudentActivity::class.java)
+        startActivity(intent)
     }
 }
