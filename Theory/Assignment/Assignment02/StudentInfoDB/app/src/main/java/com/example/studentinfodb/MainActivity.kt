10d23960d857a2bd4ff10763d6dd1abed05d91e4
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-12 23:09:34 +0600

    build #35: add various dependency
    - room
    - lifecycle (viewmodel and livemode)
    - coroutines
    - navigations
    - fragments
    - data binding

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
@@ -1,37 +1,11 @@
 package com.example.studentinfodb
 
-import android.content.Intent
-import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.example.studentinfodb.adapter.ItemAdapter
-import com.example.studentinfodb.data.Datasource
-import com.example.studentinfodb.databinding.ActivityMainBinding
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var recyclerView: RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // change into view binding
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        // initialize the data source
-        val myDataSource = Datasource().loadStudents()
-
-        // add reference for the recycle view
-        recyclerView = binding.recyclerView
-        recyclerView.adapter = ItemAdapter(this, myDataSource)
-        // improve performance for fixed dataset
-        recyclerView.setHasFixedSize(true)
-
-
-    }
-
-    fun fabButton(view: View) {
-        val intent = Intent(applicationContext, InsertStudentActivity::class.java)
-        startActivity(intent)
+        setContentView(R.layout.activity_main)
     }
 }
