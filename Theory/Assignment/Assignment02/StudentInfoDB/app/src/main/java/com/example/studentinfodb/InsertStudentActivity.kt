1cd811a26689ddd1d67134624054bfc672c9f7c2
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-12 19:35:39 +0600

    build #35: add data binding dependency
    remove the view binding dependency and add data binding dependency

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/InsertStudentActivity.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/InsertStudentActivity.kt
@@ -2,10 +2,21 @@ package com.example.studentinfodb
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.studentinfodb.databinding.ActivityInsertStudentBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class InsertStudentActivity : AppCompatActivity() {
+    private lateinit var textField: TextInputLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_insert_student)
+        // view binding the layout
+        val binding = ActivityInsertStudentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+//        val items = listOf("Option 1", "Option 2", "Option 3", "Option 4")
+//        val adapter = ArrayAdapter(requireContext(), R.layout.academic_school_list, items)
+//        (textField.editText as? AutoCompleteTextView)?.setAdapter(adapter)
     }
 }
