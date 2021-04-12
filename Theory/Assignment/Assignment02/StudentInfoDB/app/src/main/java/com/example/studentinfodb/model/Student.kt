3c4597c883af11f4cfbf83e7b1327d14eea81dba
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-12 21:47:11 +0600

    rm #35: remove data and model packages

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/model/Student.kt
+++
@@ -1,7 +0,0 @@
-package com.example.studentinfodb.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Student(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
-}
