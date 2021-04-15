b27aa68b7772cd624520c652cbc9f7db41825004
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-15 14:44:00 +0600

    feat #68: add radio button event function

--- Lab/Lab06/FragmentCodingChallenge/app/src/main/java/com/example/fragmentcodingchallenge/FeedbackFragment.java
+++ Lab/Lab06/FragmentCodingChallenge/app/src/main/java/com/example/fragmentcodingchallenge/FeedbackFragment.java
@@ -7,9 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 
 public class FeedbackFragment extends Fragment {
+
+    // radio button choice states: 0 = yes, 1 = no, 2 = no choice(default)
+    public static final int YES = 0;
+    public static final int NO = 1;
 
 
     public FeedbackFragment() {
@@ -21,6 +28,36 @@ public class FeedbackFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feedback, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_feedback, container, false);
+        // add radio group reference for the view
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+        // add rating reference for the view
+        final RatingBar ratingBar = rootView.findViewById(R.id.ratingBar);
+
+        // radio button event click listener
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group,
+                                                 int checkedId) {
+                        View radioButton = radioGroup.findViewById(checkedId);
+                        int index = radioGroup.indexOfChild(radioButton);
+                        TextView textView =
+                                rootView.findViewById(R.id.fragment_header);
+                        switch (index) {
+                            case YES: // User chose "Yes".
+                                textView.setText(R.string.yes_message);
+                                break;
+                            case NO: // User chose "No".
+                                textView.setText(R.string.no_message);
+                                break;
+                            default: // No choice made.
+                                // Do nothing.
+                                break;
+                        }
+                    }
+                });
+
+        return rootView;
     }
 }
