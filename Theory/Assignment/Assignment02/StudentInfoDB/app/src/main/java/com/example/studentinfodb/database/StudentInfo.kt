43317202dee917143c9cc15e4ed0f4fd9e62311d
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-15 21:43:55 +0600

    feat #35, #67: add student data class
    add all the necessary values for a student info

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/database/StudentInfo.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/database/StudentInfo.kt
@@ -2,4 +2,12 @@ package com.example.studentinfodb.database
 
 data class StudentInfo(
     val id: Int,
+    val name: String,
+    val school: String,
+    val department: String,
+    val date_of_birth: String,
+    val phone_number: Int,
+    val nid_number: Int,
+    val present_address: String,
+    val permanent_address: String
 )
