8710697caeb46f8175cc528f19c2fd29648545a9
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-16 15:38:18 +0600

    feat #35, #60, #67 : add student repository for insert student

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/repository/StudentRepository.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/repository/StudentRepository.kt
@@ -1,4 +1,26 @@
 package com.example.studentinfodb.repository
 
-class StudentRepository {
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import com.example.studentinfodb.database.StudentDAO
+import com.example.studentinfodb.database.StudentInfo
+
+/*
+pass the DAO instead of the whole database
+ */
+
+class StudentRepository(private val studentDAO: StudentDAO) {
+
+    val allStudents: LiveData<List<StudentInfo>> = studentDAO.loadAllStudents()
+
+    /*
+    suspend queries off  the main thread
+     */
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+
+    suspend fun insert(studentInfo: StudentInfo) {
+        studentDAO.insertStudent(studentInfo)
+    }
+
 }
