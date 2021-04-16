fa96fbafa442f2c4347bb22f1b81f1b58e66d8c2
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-16 22:14:57 +0600

    feat #35, #60, #67: add view model for student info

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/model/StudentViewModel.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/model/StudentViewModel.kt
@@ -1,4 +1,30 @@
 package com.example.studentinfodb.model
 
-class StudentViewModel {
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.studentinfodb.database.StudentDatabase
+import com.example.studentinfodb.database.StudentInfo
+import com.example.studentinfodb.repository.StudentRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class StudentViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val allStudents: LiveData<List<StudentInfo>>
+    private val repository: StudentRepository
+
+    init {
+        val studentDAO = StudentDatabase.getDatabase(application).studentDAO()
+        repository = StudentRepository(studentDAO)
+        allStudents = repository.allStudents
+    }
+
+    fun insert(studentInfo: StudentInfo) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insert(studentInfo)
+        }
+    }
+
 }
+
+
