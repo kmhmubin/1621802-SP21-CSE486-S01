d3e1603001e6cd610a8f97e0dc303405d503316f
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-16 14:43:10 +0600

    feat #35, #67: add DAO for student data

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/database/StudentDAO.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/database/StudentDAO.kt
@@ -1,7 +1,23 @@
 package com.example.studentinfodb.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface StudentDAO {
+
+    /*
+    Insert student data, if already exist then ignore
+     */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertStudent(vararg studentInfo: StudentInfo)
+
+    /*
+    Read all the date from database and show the list in ascending order
+     */
+    @Query("SELECT * FROM student_db ORDER BY studentId ASC")
+    fun loadAllStudents(): LiveData<List<StudentInfo>>
 }
