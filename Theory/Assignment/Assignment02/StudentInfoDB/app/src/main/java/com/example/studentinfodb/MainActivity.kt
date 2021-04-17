9189c3fa5315d2c426a49dbb8c0550254adf8d1d
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-17 15:01:42 +0600

    feat #35, #60: change the fragment label

--- Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
+++ Theory/Assignment/Assignment02/StudentInfoDB/app/src/main/java/com/example/studentinfodb/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.studentinfodb
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+/*
+Change the name of the action bar
+ */
+        setupActionBarWithNavController(findNavController(R.id.fragment))
     }
 
 
