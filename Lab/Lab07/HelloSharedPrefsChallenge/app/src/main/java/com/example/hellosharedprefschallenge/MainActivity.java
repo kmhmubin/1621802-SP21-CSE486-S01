9a8a3b4251b6a1ec011462284c28a593cb676c8b
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-22 21:57:52 +0600

    style #69: add custom colors and UI for activity layout

--- Lab/Lab07/HelloSharedPrefsChallenge/app/src/main/java/com/example/hellosharedprefschallenge/MainActivity.java
+++ Lab/Lab07/HelloSharedPrefsChallenge/app/src/main/java/com/example/hellosharedprefschallenge/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hellosharedprefschallenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSettingsClick(View view) {
+    }
+
+    public void reset(View view) {
+    }
+
+    public void countUp(View view) {
+    }
+
+    public void changeBackground(View view) {
     }
 }
