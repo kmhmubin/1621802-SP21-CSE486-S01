4febf1b08caa2a418cd021c5a28e26cee2c5dbd2
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-22 22:10:16 +0600

    feat #69: add save and restore data

--- Lab/Lab07/HelloSharedPrefsChallenge/app/src/main/java/com/example/hellosharedprefschallenge/MainActivity.java
+++ Lab/Lab07/HelloSharedPrefsChallenge/app/src/main/java/com/example/hellosharedprefschallenge/MainActivity.java
@@ -1,27 +1,81 @@
 package com.example.hellosharedprefschallenge;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Current count
+    private int mCount = 0;
+    // Current background color
+    private int mColor;
+    // Text view to display both count and color
+    private TextView mShowCountTextView;
+
+    // Key for current count
+    private final String COUNT_KEY = "count";
+    // Key for current color
+    private final String COLOR_KEY = "color";
+    private final String COUNT_SAVE_KEY = "count_save";
+    private String COUNT_EXTRA = "count_extra";
+    private SharedPreferences mPreferences;
+
+    private String sharedPrefFile = "com.example.android.hellosharedprefschallenge";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mShowCountTextView = findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this, R.color.default_background);
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        mCount = mPreferences.getInt(COUNT_KEY, 0);
+        mColor = mPreferences.getInt(COLOR_KEY, mColor);
+        mShowCountTextView.setText(String.format("%s", mCount));
+        mShowCountTextView.setBackgroundColor(mColor);
     }
 
     public void onSettingsClick(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 
     public void reset(View view) {
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+        mColor = mPreferences.getInt(COLOR_KEY, mColor);
+        mShowCountTextView.setBackgroundColor(mColor);
     }
 
     public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s", mCount));
     }
 
     public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = mPreferences.edit();
+        boolean countSave = mPreferences.getBoolean(COUNT_SAVE_KEY, true);
+        if (countSave) {
+            editor.putInt(COUNT_KEY, mCount);
+        } else {
+            editor.putInt(COUNT_KEY, 0);
+        }
+        editor.apply();
     }
 }
