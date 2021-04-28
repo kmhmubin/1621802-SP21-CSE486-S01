206661bff0cfa654133085cfad6c50c1b3817698
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-28 21:51:36 +0600

    feat #71, #75: add navigation transition for fragments

--- Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/MainActivity.kt
+++ Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.studentinfofbdb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.studentinfofbdb.fragments.LoginFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationHost {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        * check save instance null
+        * replace the current container [MainActivity] with [LoginFragment]
+         */
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, LoginFragment())
+                .commit()
+        }
+    }
+
+    override fun navigateTo(fragment: Fragment, addToBackStack: Boolean) {
+        val transaction = supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+
+        if (addToBackStack) {
+            transaction.addToBackStack(null)
+        }
+        transaction.commit()
     }
 }
