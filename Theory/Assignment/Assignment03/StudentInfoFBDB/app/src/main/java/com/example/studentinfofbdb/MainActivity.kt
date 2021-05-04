1de6ae976f7b1f31749274960ea766849e0caf01
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-04 17:40:02 +0600

    fix #71, #72: nav host fragment

--- Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/MainActivity.kt
+++ Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/MainActivity.kt
@@ -2,31 +2,12 @@ package com.example.studentinfofbdb
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity(), NavigationHost {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*
-        * check save instance null
-        * replace the current container [MainActivity] with [LoginFragment]
-         */
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.container, LoginFragment())
-                .commit()
-        }
-    }
-
-    override fun navigateTo(fragment: Fragment, addToBackStack: Boolean) {
-        val transaction = supportFragmentManager.beginTransaction()
-            .replace(R.id.container, fragment)
-
-        if (addToBackStack) {
-            transaction.addToBackStack(null)
-        }
-        transaction.commit()
+       
     }
 }
--- Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/fragments/list/ListFragment.kt
+++ Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/fragments/list/ListFragment.kt
@@ -1,53 +1,60 @@
 package com.example.studentinfofbdb.fragments.list
 
 import android.os.Bundle
-import android.view.*
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.studentinfofbdb.R
-import com.example.studentinfofbdb.databinding.FragmentListBinding
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class ListFragment : Fragment() {
-
-    private var _binding: FragmentListBinding? = null
-    private val binding get() = _binding!!
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentListBinding.inflate(inflater, container, false)
-        val view = binding.root
-
-        // set up the toolbar
-        (activity as AppCompatActivity).setSupportActionBar(binding.appBar)
-
-
-        // floating action button
-        binding.floatingActionButton.setOnClickListener {
-            (activity as NavigationHost).navigateTo(AddFragment(), true)
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
-        menuInflater.inflate(R.menu.toolbar_menu, menu)
-        super.onCreateOptionsMenu(menu, menuInflater)
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ListFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ListFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
 }
