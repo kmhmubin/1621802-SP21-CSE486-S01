11e90ca5f441356f8754dd6d14e7615d8a9b706c
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-05 00:56:40 +0600

    feat #71, #73: add login page logic
    Login Page:
    - check password validation
    - navigate to next fragment

--- Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/ui/login/LoginFragment.kt
+++ Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/ui/login/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.example.studentinfofbdb.ui.login
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.studentinfofbdb.R
 import com.example.studentinfofbdb.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -23,7 +26,36 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        // Set an error if the password is less than 8 characters.
+        binding.loginButton.setOnClickListener {
+            if (!isPasswordValid(binding.passwordEditText.text!!)) {
+                binding.passwordTextInput.error = getString(R.string.error_password)
+            } else {
+                // clear the error
+                binding.passwordTextInput.error = null
+                // Navigate to the list Fragment
+                Navigation.findNavController(view)
+                    .navigate(R.id.action_loginFragment_to_listFragment)
+
+            }
+        }
+
+        // Clear the error once more than 8 characters are typed.
+        binding.passwordEditText.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(binding.passwordEditText.text!!)) {
+                // clear the error
+                binding.passwordTextInput.error = null
+            }
+            false
+        }
+
+
         return view
+    }
+
+    private fun isPasswordValid(text: Editable): Boolean {
+        return text != null && text.length >= 8
     }
 
     override fun onDestroyView() {
