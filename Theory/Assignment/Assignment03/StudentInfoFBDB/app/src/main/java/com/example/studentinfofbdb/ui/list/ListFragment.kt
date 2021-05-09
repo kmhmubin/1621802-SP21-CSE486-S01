70dafaf5537d65e8cf997247c2ba467fda47fd27
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-09 22:19:02 +0600

    feat #71, #73: add school name and department name list

--- Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/ui/list/ListFragment.kt
+++ Theory/Assignment/Assignment03/StudentInfoFBDB/app/src/main/java/com/example/studentinfofbdb/ui/list/ListFragment.kt
@@ -6,16 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.studentinfofbdb.R
+import com.example.studentinfofbdb.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        return view
     }
 
 
