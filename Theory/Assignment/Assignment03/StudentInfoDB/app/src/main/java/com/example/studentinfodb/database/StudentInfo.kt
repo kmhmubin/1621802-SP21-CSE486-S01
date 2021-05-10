ddb034dfbf2b5cfb12bed9e5d503b985d5330dda
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-10 21:05:24 +0600

    fix #71, #73, #74: insert information successful
    reason:
    - using north south email for Firebase database
    
    fix:
    - using personal email for Firebase database

--- Theory/Assignment/Assignment03/StudentInfoDB/app/src/main/java/com/example/studentinfodb/database/StudentInfo.kt
+++ Theory/Assignment/Assignment03/StudentInfoDB/app/src/main/java/com/example/studentinfodb/database/StudentInfo.kt
@@ -2,22 +2,13 @@ package com.example.studentinfodb.database
 
 
 data class StudentInfo(
-
-    val studentId: String?,
-
+    val studentId: String? = null,
     var studentName: String? = null,
-
     var schoolName: String? = null,
-
     var departmentName: String? = null,
-
     var dateOfBirth: String? = null,
-
     var phoneNumber: String? = null,
-
     var nidNumber: String? = null,
-
     var presentAddress: String? = null,
-
     var permanentAddress: String? = null
 )
