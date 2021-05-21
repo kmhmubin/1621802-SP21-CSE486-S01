dd65f1cd2644d546990a45712e663c3bb82beaf9
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-22 02:09:50 +0600

    style #80, #82, #85: add recycleview with shimmer effect
    add:
    - recycle view
    - shimmer effect recycle view
    
    fix:
    - job post card texts and images position

--- Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/ui/fragments/home/HomeFragment.kt
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/ui/fragments/home/HomeFragment.kt
@@ -6,16 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nsucpcadmin.R
+import com.example.nsucpcadmin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var _binding: FragmentHomeBinding
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // show recycler view with shimmer effect
+        binding.recyclerview.showShimmer()
+
+
+
+        return view
     }
 
 }
