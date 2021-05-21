332da7629c3d2fcebbbb45281479c487e669d5be
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-21 18:31:40 +0600

    feat #82, #84: add navigation logic for bottom navbar

--- Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/MainActivity.kt
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.nsucpcadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.nsucpcadmin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.mailFragment,
+                R.id.profileFragment
+            )
+        )
+
+        // bottom navigation
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
