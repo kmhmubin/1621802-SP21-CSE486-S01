8866e7eb2d393a094dc979a55edc35af5a9607a3
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-24 01:00:23 +0600

    style #82, #80: add splash screen with Lottie animation

---
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/ui/activities/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.nsucpcadmin.ui.activities.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nsucpcadmin.R
+import com.example.nsucpcadmin.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        // lottie animation
+        binding.logoLottieAnimation.playAnimation()
+
+
+
+        setContentView(view)
+    }
+}
