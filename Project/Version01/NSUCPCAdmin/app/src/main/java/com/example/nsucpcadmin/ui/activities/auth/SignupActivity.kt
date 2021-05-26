01b1301fd2d37288b14af0a2f2e409668772d1d2
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-26 16:55:22 +0600

    feat #82, #81, #83: add Firebase auth to register the user

--- Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/ui/activities/auth/SignupActivity.kt
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/ui/activities/auth/SignupActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nsucpcadmin.databinding.ActivitySignupBinding
 import com.example.nsucpcadmin.ui.activities.base.BaseActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignupActivity : BaseActivity() {
 
@@ -71,17 +75,52 @@ class SignupActivity : BaseActivity() {
                 false
             }
             else -> {
-                showSnackBar("Your details are valid", false)
                 true
             }
 
         }
     }
 
-    
 
+    /*
+    * function to register the user with email and password using FirebaseAuth
+     */
 
+    private fun registerUser() {
+        // check the validation if the entries are valid or not
+        if (validateRegisterDetails()) {
+            // show the progress dialog
+            showProgressBar()
 
+            val email: String = binding.signupUserEmailEditText.text.toString().trim()
+            val password: String = binding.signupUserPasswordEditText.text.toString().trim()
+
+            // create an instance and register user with email and password
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+                        // hide the progress bar
+                        hideProgressBar()
+
+                        // if register is successful
+                        if (task.isSuccessful) {
+                            // Firebase register user
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                            // show the registration success message
+                            showSnackBar("Registration Successful", false)
+                            // after registration complete then logout and send to login screen
+                            FirebaseAuth.getInstance().signOut()
+                            // finish the registration screen
+                            finish()
+                        } else {
+                            // show error message
+                            showSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    }
+                )
+        }
+    }
 
 
 }
