e900a6d131bc143601424b97e55ebf0e508915b0
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-26 19:16:41 +0600

    feat #83: add function for get logged user details

--- Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/data/firebase/FirebaseSource.kt
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/data/firebase/FirebaseSource.kt
@@ -1,6 +1,11 @@
 package com.example.nsucpcadmin.data.firebase
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
 import com.example.nsucpcadmin.data.model.AdminUser
+import com.example.nsucpcadmin.ui.activities.auth.LoginActivity
 import com.example.nsucpcadmin.ui.activities.auth.SignupActivity
 import com.example.nsucpcadmin.utils.Constants
 import com.google.firebase.auth.FirebaseAuth
@@ -50,6 +55,55 @@ class FirebaseSource {
             currentUserID = currentUser.uid
         }
         return currentUserID
+    }
+
+    /*
+    * Function for get logged user details from Firestore databse
+     */
+
+    fun getUserDetails(activity: Activity) {
+        // pass the collection name, to get data from
+        mFireStore.collection(Constants.USER)
+            .document(getCurrentUserID())
+            .get()
+            .addOnSuccessListener { document ->
+                // print in the log
+                Log.i(activity.javaClass.simpleName, document.toString())
+
+                // receive the document snapshot and convert in user data model object
+                val user = document.toObject(AdminUser::class.java)!!
+
+                // save in shared prefs
+                val sharedPreferences = activity.getSharedPreferences(
+                    Constants.NSU_CPC_ADMIN_PREFERENCES,
+                    Context.MODE_PRIVATE
+                )
+
+                // Instance of the editor which is help us to edit
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                editor.putString(
+                    Constants.LOGGED_IN_USERNAME, "${user.name}"
+                )
+                editor.apply()
+
+                when (activity) {
+                    is LoginActivity -> {
+                        // call a function of base activity for transferring tha result
+                        activity.userLoggedInSuccess(user)
+                    }
+                }
+            }
+            .addOnFailureListener { e ->
+                when (activity) {
+                    is LoginActivity -> {
+                        activity.hideProgressBar()
+                    }
+                }
+                // save the error logs
+                FirebaseCrashlytics.getInstance().log("Error while getting user details")
+                FirebaseCrashlytics.getInstance().setCustomKey("Login Error", e.toString())
+                FirebaseCrashlytics.getInstance().recordException(e)
+            }
     }
 
 
