14278ffa949863a0ae11ef825a9ae5c6297bab85
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-26 19:51:44 +0600

    style #80, #82: add custom radio button

---
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/ui/activities/editprofile/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nsucpcadmin.ui.activities.editprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nsucpcadmin.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
