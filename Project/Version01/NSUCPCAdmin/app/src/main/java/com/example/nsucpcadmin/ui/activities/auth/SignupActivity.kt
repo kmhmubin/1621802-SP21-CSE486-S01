cc6af259d1eb9fefe9ae683f44314f24ddfeb410
K M H Mubin <kmh.mubin@northsouth.edu>
2021-05-26 15:26:32 +0600

    feat #82, #81 : add register form validation

--- Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/ui/activities/auth/SignupActivity.kt
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/ui/activities/auth/SignupActivity.kt
@@ -22,6 +22,66 @@ class SignupActivity : BaseActivity() {
             finish()
         }
 
+        /*
+        * Register button click function
+         */
+        binding.signupButton.setOnClickListener {
+            validateRegisterDetails()
+        }
+
         setContentView(view)
     }
+
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            // name
+            binding.signupUserNameEditText.text.toString().trim().isEmpty() -> {
+                binding.signupUserNameTextLayout.error = "Please enter your name"
+                false
+            }
+            //email
+            binding.signupUserEmailEditText.text.toString().trim().isEmpty() -> {
+                binding.signupUserEmailTextLayout.error = "Please enter valid North South email"
+                false
+            }
+            // id
+            binding.signupUserIDEditText.text.toString().trim().isEmpty() -> {
+                binding.signupUserIDTextLayout.error = "Please enter your 10 digit NSU ID"
+                false
+            }
+            // password
+            binding.signupUserPasswordEditText.text.toString().trim().isEmpty() -> {
+                binding.signupUserPasswordTextLayout.error = "Please enter password"
+                false
+            }
+            binding.signupUserPasswordEditText.text.toString().length < 4 -> {
+                binding.signupUserPasswordTextLayout.error =
+                    "Password must contain at least 5 characters"
+                false
+            }
+            // confirm password
+            binding.signupUserConfirmPasswordEditText.text.toString().trim().isEmpty() -> {
+                binding.signupUserConfirmPasswordTextLayout.error =
+                    "Please enter your password again"
+                false
+            }
+            binding.signupUserConfirmPasswordEditText.text.toString() != binding.signupUserPasswordEditText.text.toString() -> {
+                binding.signupUserConfirmPasswordTextLayout.error =
+                    "Password & confirm password does not match"
+                false
+            }
+            else -> {
+                showSnackBar("Your details are valid", false)
+                true
+            }
+
+        }
+    }
+
+    
+
+
+
+
+
 }
